2de40a9d946bac2381447044d47b62863d6048d0
Mateusz Serdynski <mateusz.serdynski@gmail.com>
2016-02-03 22:14:40 +0100

    In town only 1 panel active

---
+++ Assets/Scripts/Buildings/BuildingPanelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingPanelManager : MonoBehaviour {
+
+    public GameObject quarryPanel;
+    public GameObject lumberjackPanel;
+    public GameObject closePanelsPanel;
+
+	public void CloseAllPanels()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    public void OpenLuberjackPanel()
+    {
+        lumberjackPanel.SetActive(true);
+        closePanelsPanel.SetActive(true);
+    }
+
+    public void OpenQuyarryPanel()
+    {
+        quarryPanel.SetActive(true);
+        closePanelsPanel.SetActive(true);
+    }
+}
